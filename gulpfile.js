43267976603a0967332b499f4f894ee81aad937a
Florian Pichler <f.pichler@anfe.ma>
2018-02-18 17:06:42 +0100

    CHORE: Update tooling

--- gulpfile.js
+++ gulpfile.js
@@ -4,62 +4,75 @@ const cssnano = require('cssnano');
 const del = require('del');
 const mqpacker = require('css-mqpacker');
 const postcss = require('gulp-postcss');
-const rename = require('gulp-rename');
+const rollup = require('rollup');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const { dest, parallel, src, series, task, watch } = require('gulp');
 
-task('clean', () => del(['./tmp']));
+task('clean', () => del(['./dist']));
 
-task('styles', () => src('styles/main.scss')
-	.pipe(sourcemaps.init({
-		loadMaps: true,
-	}))
-	.pipe(sass({
+task('styles', () =>
+	src('src/styles/main.scss')
+		.pipe(
+			sourcemaps.init({
+				loadMaps: true,
+			})
+		)
+		.pipe(
+			sass({
+				sourcemap: true,
+			}).on('error', sass.logError)
+		)
+		.pipe(
+			postcss([
+				autoprefixer({
+					browsers: ['last 1 versions'],
+					flexbox: 'no-2009',
+				}),
+				mqpacker({
+					sort: true,
+				}),
+				cssnano({
+					autoprefixer: false,
+				}),
+			])
+		)
+		.pipe(sourcemaps.write('.'))
+		.pipe(dest('dist'))
+		.pipe(browserSync.stream())
+);
+
+task('scripts', async () => {
+	const bundle = await rollup.rollup({
+		input: 'src/scripts/main.js',
+		plugins: [],
+		banner: '/*! florianpichler.de */',
+	});
+
+	await bundle.write({
+		file: 'dist/main.js',
+		format: 'iife',
 		sourcemap: true,
-	})
-	.on('error', sass.logError))
-	.pipe(postcss([
-		autoprefixer({
-			browsers: ['last 2 versions'],
-		}),
-		mqpacker({
-			sort: true,
-		}),
-		cssnano({
-			autoprefixer: false,
-		}),
-	]))
-	.pipe(sourcemaps.write('.'))
-	.pipe(dest('tmp')));
+	});
 
-task('content', () => src('content/**/*.hbs')
-	.pipe(rename((p) => {
-		p.extname = '.html';
-	}))
-	.pipe(dest('tmp')));
-
-task('build', parallel('content', 'styles'));
-
-task('reload', (done) => {
 	browserSync.reload();
-
-	return done();
 });
+
+task('content', () => src('src/*.{html,php}').pipe(dest('dist')));
+
+task('build', parallel('content', 'scripts', 'styles'));
 
 task('watch', () => {
 	browserSync.init({
 		ghostMode: false,
-		server: {
-			baseDir: ['tmp', 'static'],
-		},
+		server: 'dist',
 		notify: false,
-		injectChanges: false,
 		open: false,
 	});
 
-	watch('content/**/*', series('content', 'reload'));
-	watch('styles/**/*.scss', series('styles', 'reload'));
+	watch('src/*.{html,php}', series('content')).on('change', browserSync.reload);
+	watch('src/styles/**/*.scss', series('styles'));
+	watch('src/scripts/**/*.js', series('scripts'));
 });
 
-task('default', series('build', 'watch'));
+task('default', series('clean', 'build', 'watch'));
