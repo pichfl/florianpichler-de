a92969fea700f21a2b9964a3cbf4ccd3cbc170a1
Florian Pichler <f.pichler@anfe.ma>
2018-02-18 17:07:14 +0100

    FEATURE: Layout for disabled JavaScript

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+// const d = document;
+// const $ = query => d.querySelector(query);
+
+// const card = d.createElement('div');
+// const header = $('header');
+
+// card.append(header);
+// card.append(header.cloneNode(true));
+// card.append($('.contact'));
+
+// document.body.prepend(card);
+
+// console.log(card);
