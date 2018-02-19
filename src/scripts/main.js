a472c129fb27c789250ae6753e43e413abe7dac2
Florian Pichler <f.pichler@anfe.ma>
2018-02-19 23:07:53 +0100

    FEATURE: More elaborate rotation
    Adding a bit of z rotation to the mix gives the turnover a lot more dynamic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,43 @@
-// const d = document;
-// const $ = query => d.querySelector(query);
+const d = document;
+const $ = query => d.querySelector(query);
+const create = el => document.createElement(el);
 
-// const card = d.createElement('div');
-// const header = $('header');
+const card = $('.card');
+const cardWrap = create('div');
+const front = create('button');
+const back = create('button');
+const tagLine = $('.card header p').cloneNode(true);
+const logo = create('div');
 
-// card.append(header);
-// card.append(header.cloneNode(true));
-// card.append($('.contact'));
+cardWrap.className = 'wrap';
+front.className = 'card front';
+logo.className = 'logo';
 
-// document.body.prepend(card);
+back.title = 'Flip';
+back.innerText = '←';
 
-// console.log(card);
+back.setAttribute('aria-hidden', true);
+front.setAttribute('aria-hidden', true);
+card.classList.add('back');
+
+card.append(back);
+front.append(logo);
+front.append(tagLine);
+cardWrap.append(front);
+cardWrap.append(card);
+
+document.body.prepend(cardWrap);
+
+front.addEventListener('click', () => {
+	cardWrap.classList.add('flip');
+	cardWrap.classList.remove('unflip');
+	document.body.classList.add('flipped');
+});
+
+back.addEventListener('click', () => {
+	cardWrap.classList.add('unflip');
+	cardWrap.classList.remove('flip');
+	document.body.classList.remove('flipped');
+});
+
+cardWrap.addEventListener('animationend', () => console.log('ended'));
